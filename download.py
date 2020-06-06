07908e04b261ad5a16e9a4aae257526a6df8cd70
Ravbug <ravbuganimations@gmail.com>
2020-06-06 14:29:38 -0700

    Different method for downloading the spreadsheets

--- download.py
+++ download.py
@@ -3,26 +3,14 @@
 import argparse
 parser = argparse.ArgumentParser(description='Download the translation spreadsheet and write it as a TSV file')
 parser.add_argument('--id', type=str,help='The ID of the spreadsheet')
+parser.add_argument('--format',type=str,help="The format to export as",default="tsv")
 args = parser.parse_args()
-
 
 # download the spreadsheet from google as a TSV
 import requests
-sheetid = args.id
 print("Downloading file")
-data = requests.get("https://docs.google.com/spreadsheets/d/"+ sheetid +"/gviz/tq?tq=SELECT%20C").text
+data = requests.get("https://docs.google.com/spreadsheets/d/" + args.id + "/export?gid=0&format="+args.format).text
 
-# parse the JSON
-print("Parsing JSON")
-import json
-table = json.loads(data[data.index('{'):-2])
-
-# combine it into an array
-tsv = []
-for cel in table["table"]["rows"]:
-    tsv.append("\"" + cel['c'][0]['v'] + "\"")
-
-# write to file
-print("Writing translated.tsv")
-with open("translated.tsv","w") as f:
-    f.write("\r\n".join(tsv))
+print("Writing translated."+args.format)
+with open("translated." + args.format,"w") as f:
+    f.write(data)
